3464257de867fa9262f49ba24e3e2efb27f8f149
naegonggae <https://github.com/naegonggae/>
2022-11-24 00:18:22 +0900

    first commit 1. application.yml 수정

---
+++ src/main/java/com/mustache/bbsproject3/MustacheBbsProject3Application.java
@@ -0,0 +1,13 @@
+package com.mustache.bbsproject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MustacheBbsProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MustacheBbsProject3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mustache/bbsproject3/MustacheBbsProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustache.bbsproject3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MustacheBbsProject3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
